9a2e5b6bf89bdf32ebb4dd4f8531377ee34c557c
jenningstopps <jenningsT@live.com>
2020-12-04 16:48:06 -0800

    Update cpeproj.ino

--- cpeproj/cpeproj.ino
+++ cpeproj/cpeproj.ino
@@ -1,10 +1,22 @@
-//Monitor the water levels in a reservoir and print an alert when the level is too low
-//Monitor and display the current air temp and humidity on an LCD screen
-//Start and stop a fan motor as needed when the temperature falls out of a specified range(high or low)
-//Allow a user to use a control to adjust the angle of an output vent from the system
-//Allow as user to enable or disable the system using an on/off button
-//Record the time and date every time the motor is turned on or off. This information should 
-  //be transmitted to a host computer (over USB)
+/* 
+ 1) Monitor the water levels in a reservoir and print an alert when the level is too low
+    - use water level detection sensor module
+ 2) Monitor and display the current air temp and humidity on an LCD screen
+    - use DHT11 Temperature and Humidity Module
+ 3) Start and stop a fan motor as needed when the temperature falls out of a specified range(high or low)
+    - use Fan blade and 3-6V Motor
+ 4) Allow a user to use a control to adjust the angle of an output vent from the system
+    - 
+ 5) Allow as user to enable or disable the system using an on/off button
+    - use push button 
+ 6) Record the time and date every time the motor is turned on or off. This information should 
+     be transmitted to a host computer (over USB)
+
+ extra:
+    - at some point an analog signal needs to be converted to digital
+*/
+
+
 
 void setup() {
   
